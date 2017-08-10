56d2bc1691ad430cb76d3631e15d448734740993
tioguda <guda.flavio@gmail.com>
2017-08-09 21:28:39 -0300

    BringUp Validus

--- validus.mk
+++ validus.mk
@@ -1,15 +1,18 @@
-
 # Inherit from common
-$(call inherit-product, device/samsung/j5-common/lineage.mk)
+$(call inherit-product, device/samsung/j5-common/validus.mk)
 
 $(call inherit-product, device/samsung/j5ltechn/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j5ltechn
-PRODUCT_NAME := lineage_j5ltechn
+PRODUCT_NAME := validus_j5ltechn
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-J5008
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_CHARACTERISTICS := phone
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+# Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="tioguda"
